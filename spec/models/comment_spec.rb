89bd2b254352212d1eca61d6579d9ca416016a58
Jimmy Adames <jimmy.adames@gmail.com>
2017-09-14 18:40:44 -0400

    Changed user to creator in the associations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  it {should belong_to :user}
+  it {should belong_to :creator}
   it {should belong_to :post}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe Post, type: :model do
-  it {should belong_to :user}
+  it {should belong_to :creator}
   it {should have_many :comments}
 end
