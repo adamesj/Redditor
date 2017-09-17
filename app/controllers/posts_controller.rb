054ad671cf9c991af439245ff45bc4e5f9ea833f
Jimmy Adames <jimmy.adames@gmail.com>
2017-09-17 13:10:50 -0400

    Build out the remaining logic for the posts controller and added model tests for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,52 @@
 class PostsController < ApplicationController
+  before_action  :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "You've created a new post!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Something went wrong, check the error message."
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @post.update_attribtues(post_params)
+      flash[:notice] = "Your post has been updated!"
+      redirect_to post_path(post)
+    else
+      flash[:error] = "Something went wrong, check the error message."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "Your post has been deleted."
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :url)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,18 @@ describe Post, type: :model do
   it {should have_many :comments}
   it {should have_many :post_categories}
   it {should have_many :categories}
+
+  it "should have a title with a minimum of 5 characters" do
+    post = Post.new(title: "test article", description: "some description", url: "url.com")
+    post.title = "a" * 3
+    post.valid?
+    expect(post.errors[:title]).to include("is too short (minimum is 5 characters)")
+  end
+
+  it "is invalid without a unique url" do
+    Post.create(title: "some title", url: "website.com")
+    post = Post.new(title: "another title", url: "website.com")
+    post.valid?
+    expect(post.errors[:url]).to include("has already been taken")
+  end
 end
